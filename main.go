ed19c2c6c31bbaabe0e21e2c5065e9aa7e0a7c9a
Claudia De la Cerda <claudia.delacerda7880@alumnos.udg.mx>
2020-10-15 23:42:21 -0500

    Prueba 01

--- main.go
+++ main.go
@@ -2,6 +2,22 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	var n int
+	var m int
+	var sum int
+
+	fmt.Scan(&n)
+	s := make([]int, 0, n)
+
+	for i := 0; i < n; i++ {
+		fmt.Scan(&m)
+		s = append(s, m)
+	}
+
+	for _, v := range s {
+		sum += v
+	}
+
+	fmt.Println(sum)
 }
